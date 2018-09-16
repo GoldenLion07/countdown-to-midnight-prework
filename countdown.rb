97d8beac91f24f38c4aed07691ed3d0f3016cad6
Scott M <gmat191@gmail.com>
2018-09-16 00:45:28 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -10,9 +10,11 @@ end
 
 def countdown_with_sleep(num)
   while num > 0 
-  sleep 5
+  sleep 5 # With Sleep the countdown should take 5 seconds to execute.
   puts "#{num} SECOND(S)!"
   num -= 1
 end
 return "HAPPY NEW YEAR!"
 end
+
+#-=
